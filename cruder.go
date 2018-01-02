7e142c5bca00c0894e96fd96bd8bdaa59f4ee36e
Saeed Falsafin <sabloger@gmail.com>
2018-01-02 18:06:38 +0330

    +update

--- cruder.go
+++ cruder.go
@@ -3,6 +3,8 @@ package gorm2gin
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+	"strconv"
+	"reflect"
 )
 
 type CRUDerModelInterface interface {
@@ -22,16 +24,55 @@ func InitCRUDer(db *gorm.DB, model CRUDerModelInterface) *CRUDer {
 	return cruder
 }
 
-func (CRUDer *CRUDer) List(c *gin.Context) {
-	var res = CRUDer.m.NewSlice()
-	CRUDer.db.Limit(100).Find(res)
-	c.JSON(200, res)
+func (CRUDer *CRUDer) List(context *gin.Context) {
+	var (
+		res                                       = CRUDer.m.NewSlice()
+		pageIndex, pageSize                       int
+		pageIndexStr, hasPageIndex                = context.GetQuery("page_index")
+		pageSizeStr, hasPageSize                  = context.GetQuery("page_size")
+		pageOrderField, hasPageOrderField         = context.GetQuery("page_order_field")
+		pageOrderDirection, hasPageOrderDirection = context.GetQuery("page_order_direction")
+	)
+
+	if !hasPageIndex {
+		pageIndex = 1
+	} else {
+		pageIndex, _ = strconv.Atoi(pageIndexStr)
+	}
+	if !hasPageSize {
+		pageSize = 10
+	} else {
+		pageSize, _ = strconv.Atoi(pageSizeStr)
+	}
+	if !hasPageOrderField || pageOrderField == "" {
+		pageOrderField = "id"
+	}
+	if !hasPageOrderDirection {
+		pageOrderDirection = "asc"
+	}
+	CRUDer.db.Offset(pageIndex - 1*pageSize).Limit(pageSize).Order(pageOrderField + " " + pageOrderDirection).Find(res)
+
+	context.JSON(200, res)
 }
 
-func (CRUDer *CRUDer) Read(c *gin.Context) {
-	var id = c.Param("rid")
-	var res = CRUDer.m.NewOne()
+func (CRUDer *CRUDer) Read(context *gin.Context) {
+	var (
+		id  = context.Param("rid")
+		res = CRUDer.m.NewOne()
+	)
 	CRUDer.db.Find(res, id)
-	c.JSON(200, res)
+	context.JSON(200, res)
 }
 
+func (CRUDer *CRUDer) Update(context *gin.Context) {
+	var (
+		res      = CRUDer.m.NewOne()
+		id       = context.Param("rid")
+		intId, _ = strconv.Atoi(id)
+	)
+	var query = CRUDer.db.Find(res, id)
+	context.BindJSON(res)
+	reflect.Indirect(reflect.ValueOf(res)).FieldByName("ID").SetInt(int64(intId))
+	query.Update(res)
+	context.JSON(200, res)
+}
