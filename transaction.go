c88297bb8a491df4dc6791cbcee3058767921388
Saeed Falsafin <sabloger@gmail.com>
2018-01-22 13:40:13 +0330

    Transaction system Base is done! with distroy dead trs after 2 secs! and support multy trs concurrent!

---
+++ transaction.go
@@ -0,0 +1,64 @@
+package gorm2gin
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/gin-gonic/gin"
+	"strconv"
+	"time"
+	"fmt"
+)
+
+const TrLifeTime = time.Second * 2
+
+type Transaction struct {
+	DB *gorm.DB
+}
+
+type transactions map[uint64]*Transaction
+
+var Transactions = transactions{}
+
+func (CRUDer *CRUDer) GetOrNewTransaction(id uint64) *Transaction {
+	if tr, ok := Transactions[id]; ok {
+		return tr
+	} else { // Begin:
+		Transactions[id] = new(Transaction)
+		Transactions[id].DB = CRUDer.db.Begin()
+		go CRUDer.DestroyDeadTr(time.Tick(TrLifeTime), id)
+		return Transactions[id]
+	}
+}
+
+func (CRUDer *CRUDer) DestroyDeadTr(c <-chan time.Time, id uint64) {
+	<-c
+	if tr, ok := Transactions[id]; ok {
+		tr.DB.Rollback()
+		delete(Transactions, id)
+		fmt.Println("Dead transaction:", id)
+	}
+}
+
+func (CRUDer *CRUDer) CommitTransaction(context *gin.Context) {
+	var trUid, _ = context.GetQuery("_tr_uid")
+	var id, err = strconv.ParseUint(trUid, 10, 64)
+	if tr, ok := Transactions[id]; err != nil || !ok {
+		context.Status(400)
+	} else if err := tr.DB.Commit().Error; err != nil {
+		tr.DB.Rollback()
+		context.Status(500)
+	} else {
+		delete(Transactions, id)
+		context.Status(200)
+	}
+}
+func (CRUDer *CRUDer) RollbackTransaction(context *gin.Context) {
+	var trUid, _ = context.GetQuery("_tr_uid")
+	var id, err = strconv.ParseUint(trUid, 10, 64)
+	if tr, ok := Transactions[id]; err != nil || !ok {
+		context.Status(400)
+	} else {
+		tr.DB.Rollback()
+		delete(Transactions, id)
+		context.Status(200)
+	}
+}
---
+++ transaction_test.go
@@ -0,0 +1,96 @@
+package gorm2gin
+
+import (
+	"testing"
+	"time"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"strconv"
+	"fmt"
+	"sync"
+)
+
+var Database, _ = gorm.Open("mysql", "root:123456@/testing?charset=utf8&parseTime=True&loc=Local")
+
+func init() {
+	Database.AutoMigrate(&TestTable{})
+}
+
+type TestTable struct {
+	ID           *int64     `gorm:"primary_key;auto_increment" json:"id"`
+	Name, Family string
+	CreatedAt    *time.Time `json:"created_at"`
+	UpdatedAt    *time.Time `json:"updated_at"`
+	DeletedAt    *time.Time `json:"deleted_at"`
+}
+
+func (v *TestTable) NewOne() interface{} {
+	return new(TestTable)
+}
+func (v *TestTable) NewSlice() interface{} {
+	return new([]TestTable)
+}
+
+func TestCRUDer_GetOrNewTransaction(t *testing.T) {
+	var TestTableCRUD = InitCRUDer(Database, new(TestTable))
+	var TestTableCRUD2 = InitCRUDer(Database, new(TestTable))
+	var tr = TestTableCRUD.GetOrNewTransaction(123456)
+	//Database.Create(&TestTable{
+	tr.DB.Create(&TestTable{
+		Name:   "Saeed",
+		Family: "Falsafin",
+	})
+
+	Database.Create(&TestTable{
+		Name:   "Ali",
+		Family: "Falsafin",
+	})
+
+	var tr12 = TestTableCRUD.GetOrNewTransaction(123456)
+
+	tr12.DB.Create(&TestTable{
+		Name:   "Saeede",
+		Family: "Falsafin",
+	})
+
+
+	var tr2 = TestTableCRUD.GetOrNewTransaction(321321)
+
+	tr2.DB.Create(&TestTable{
+		Name:   "NADER",
+		Family: "Falsafin",
+	})
+	var tr3 = TestTableCRUD2.GetOrNewTransaction(321321)
+
+	tr3.DB.Create(&TestTable{
+		Name:   "NADER",
+		Family: "Haderi",
+	})
+
+	//tr12.DB.Commit()
+	//tr3.DB.Commit()
+	tr.DB.Rollback()
+	tr2.DB.Commit()
+}
+
+func TestID(t *testing.T) {
+	fmt.Println(strconv.ParseUint("1590206877993462", 10, 64))
+	fmt.Println(strconv.ParseUint("5a648e41509bc", 16, 64))
+
+}
+var w =new(sync.WaitGroup)
+
+func TestTick(t *testing.T) {
+
+	w.Add(1)
+	go tt(time.Tick(time.Second*2))
+
+	fmt.Println("salam")
+	w.Wait()
+}
+
+func tt(c <-chan time.Time )  {
+	<- c
+	fmt.Println("2 Secs later")
+	w.Done()
+}
